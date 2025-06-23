6dbee14ea0812b6c907eb32045ef98a1a712103f
Jeffrey Lee <jkclee123@gmail.com>
2025-06-23 16:49:45 +0800

    symbol formatter

--- src/utils/symbolFormatter.js
+++ src/utils/symbolFormatter.js
@@ -3,7 +3,7 @@ export const formatCodeForDisplay = (codeText) => {
     .replace(/₁₀\^\(/g, '<span class="math-symbol"><span class="subscript">10</span>^(</span>')
     .replace(/M⁺/g, '<span class="math-symbol">M+</span>')
     .replace(/M⁻/g, '<span class="math-symbol">M-</span>')
-    .replace(/⁻/g, '<span class="math-symbol   superscript">-</span>')
+    .replace(/⁻¹/g, '<span class="math-symbol"><span class="superscript">-</span>¹</span>')
     .replace(/DT/g, '<span class="math-symbol subscript">DT</span>')
     .replace(/Z₀/g, '<span class="math-symbol">Z<span class="subscript">₀</span></span>')
 }; 
